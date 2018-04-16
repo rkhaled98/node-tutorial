41f175e3f9f59ebc4fbfa2b878638d5fc42c01b0
Rafi Khaled <rkhaled@vassar.edu>
2018-04-16 01:54:37 -0400

    split up files for my_parser

--- my_parser.js
+++ my_parser.js
@@ -1,33 +1,24 @@
 
- // Load the fs (filesystem) module
- var fs = require('fs');
+// Require my new parser.js file.
+var Parser = require('./parser');
 
- // Read the contents of the file into memory.
- fs.readFile('example_log.txt', function(err, logData) {
+// Load the fs (filesystem) module.
+var fs = require('fs');
 
-     // If an error occurred, throwing it will
-     // display the exception and end our app.
-     if (err) throw err;
+// Read the contents of the file into memory.
+fs.readFile('example_log.txt', function(err, logData) {
 
-     // logData is a Buffer, convert to string.
-     var text = logData.toString();
+// If an error occurred, throwing it will
+// display the exception and kill our app.
+if (err) throw err;
 
-     var results = {};
+// logData is a Buffer, convert to string.
+var text = logData.toString();
 
-     var lines = text.split('\n');
-     lines.forEach(function(line) {
-       var parts = line.split(' ');
-       var letter = parts[1]
-       var count = parseInt(parts[2]);
+// Create an instance of the Parser object.
+var parser = new Parser();
 
-       if(!results[letter]) {
-         results[letter] = 0;
-       }
-
-       results[letter] += parseInt(count);
-
-     });
-
-     console.log(results);
-
- });
+// Call the parse function.
+console.log(parser.parse(text));
+// { A: 2, B: 14, C: 6 }
+});    
