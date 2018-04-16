6f7cf38ff17f776640167f57f63f13cac381ecd5
Rafi Khaled <rkhaled@vassar.edu>
2018-04-16 01:20:55 -0400

    my_parser complete

--- my_parser.js
+++ my_parser.js
@@ -11,4 +11,23 @@
 
      // logData is a Buffer, convert to string.
      var text = logData.toString();
+
+     var results = {};
+
+     var lines = text.split('\n');
+     lines.forEach(function(line) {
+       var parts = line.split(' ');
+       var letter = parts[1]
+       var count = parseInt(parts[2]);
+
+       if(!results[letter]) {
+         results[letter] = 0;
+       }
+
+       results[letter] += parseInt(count);
+
+     });
+
+     console.log(results);
+
  });
